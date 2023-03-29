18ce8e0beeeb34db14d0ab445e129043765fc4c5
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-29 09:45:54 -0500

    uso de While

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+        int i=1;
+        while (i<=100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
